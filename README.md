ea65d9eef73c799e4b32aa1aa3a7b4521ab7788d
manwax <regameymanuel@gmail.com>
2024-11-03 20:28:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Killmi_Tale
---
+++ main.js
@@ -0,0 +1,28 @@
+const TILE_SIZE = 64;
+const COLS = 15;
+const ROWS = 20;
+const GAME_WIDTH = TILE_SIZE * COLS;
+const GAME_HEIGHT = TILE_SIZE * ROWS;
+
+window.addEventListener('load', function(){
+    const canvas  = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = GAME_WIDTH;
+    canvas.height = GAME_HEIGHT;
+
+    function drawGrid(){
+for (let row = 0; row < ROWS; row ++){
+    for (let col = 0; col < COLS; col ++){
+        //ctx.fillRect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        ctx.strokeRect(
+            col * TILE_SIZE, 
+            row * TILE_SIZE, 
+            TILE_SIZE, 
+            TILE_SIZE
+        );
+    }}
+
+
+        }
+    drawGrid();
+})
